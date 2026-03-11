9020fb0ed730169c119ca35d39773bca1f87f298
Akira Takagi <tkgakrwork@gmail.com>
2026-03-11 21:59:29 +0900

    Title値オブジェクトの修正

--- CatalogService/src/Domain/models/Book/Title/Title.ts
+++ CatalogService/src/Domain/models/Book/Title/Title.ts
@@ -9,7 +9,7 @@ export class Title extends ValueObject<TitleValue, "Title"> {
     super(value);
   }
 
-  protected validate(value: string): void {
+  protected validate(value: TitleValue): void {
     if (value.length < Title.MIN_LENGTH || value.length > Title.MAX_LENGTH) {
       throw new Error(
         `タイトルは${Title.MIN_LENGTH}文字以上、${Title.MAX_LENGTH}文字以下でなければなりません。`,
