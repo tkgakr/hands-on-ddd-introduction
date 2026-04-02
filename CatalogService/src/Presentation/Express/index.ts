1d541fdbf77ca64404dd008f33eb10aa043e3d7c
Akira Takagi <tkgakrwork@gmail.com>
2026-04-02 22:50:44 +0900

    15-2-2 Web APIの実装

--- CatalogService/src/Presentation/Express/index.ts
+++ CatalogService/src/Presentation/Express/index.ts
@@ -1,4 +1,12 @@
 import {
+  RegisterBookCommand,
+  RegisterBookService,
+} from "Application/Book/RegisterBookService/RegisterBookService";
+import {
+  AddReviewCommand,
+  AddReviewService,
+} from "Application/Review/AddReviewService/AddReviewService";
+import {
   GetRecommendedBooksCommand,
   GetRecommendedBooksService,
 } from "Application/Review/GetRecommendedBooksService/GetRecommendedBooksService";
@@ -7,6 +15,14 @@ import { SQLBookRepository } from "Infrastructure/SQL/Book/SQLBookRepository";
 import { SQLReviewRepository } from "Infrastructure/SQL/Review/SQLReviewRepository";
 import { SQLClientManager } from "Infrastructure/SQL/SQLClientManager";
 import { SQLTransactionManager } from "Infrastructure/SQL/SQLTransactionManager";
+import {
+  EditReviewCommand,
+  EditReviewService,
+} from "Application/Review/EditReviewService/EditReviewService";
+import {
+  DeleteReviewCommand,
+  DeleteReviewService,
+} from "Application/Review/DeleteReviewService/DeleteReviewService";
 
 const app = express();
 const port = 3000;
@@ -43,4 +59,93 @@ app.get("/book/:isbn/recommendations", async (req, res) => {
   } catch {
     res.status(500).json({ ok: false });
   }
+});
+
+// 書籍登録
+app.post("/book", async (req, res) => {
+  try {
+    const { isbn, title, author, price } = req.body;
+
+    if (!isStr(isbn) || !isStr(title) || !isStr(author) || !isNum(price)) {
+      return invalid(res);
+    }
+
+    const service = new RegisterBookService(bookRepository, transactionManager);
+    const command: RegisterBookCommand = { isbn, title, author, price };
+    const book = await service.execute(command);
+
+    res.status(201).json({ ok: true, book });
+  } catch {
+    res.status(500).json({ ok: false });
+  }
+});
+
+// レビュー投稿
+app.post("/book/:isbn/review", async (req, res) => {
+  try {
+    const { isbn } = req.params;
+    const { name, rating, comment } = req.body;
+
+    if (!isStr(isbn) || !isStr(name) || !isNum(rating)) return invalid(res);
+    // 空文字はOK, 数値, 真偽値, オブジェクト, 配列は NG
+    if (comment && !isStr(comment)) return invalid(res);
+
+    const service = new AddReviewService(
+      reviewRepository,
+      bookRepository,
+      transactionManager,
+    );
+    const command: AddReviewCommand = { bookId: isbn, name, rating, comment };
+    const review = await service.execute(command);
+
+    res.status(201).json({ ok: true, review });
+  } catch {
+    res.status(500).json({ ok: false });
+  }
+});
+
+// レビュー編集
+app.put("/review/:reviewId", async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const { name, rating, comment } = req.body;
+
+    if (!isStr(reviewId)) return invalid(res);
+    // ここでチェックしているのは文字列として正しいかだけで、空文字でも通る。
+    // (桁数チェックはドメイン層にて)
+    if (name && !isStr(name)) return invalid(res);
+    if (rating && !isNum(rating)) return invalid(res);
+    if (comment && !isStr(comment)) return invalid(res);
+
+    const service = new EditReviewService(reviewRepository, transactionManager);
+    const command: EditReviewCommand = { reviewId, name, rating, comment };
+    const review = await service.execute(command);
+
+    res.status(200).json({ ok: true, review });
+  } catch {
+    res.status(500).json({ ok: false });
+  }
+});
+
+// レビュー削除
+app.delete("/review/:reviewId", async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    if (!isStr(reviewId)) return invalid(res);
+
+    const service = new DeleteReviewService(
+      reviewRepository,
+      transactionManager,
+    );
+    const command: DeleteReviewCommand = { reviewId };
+    await service.execute(command);
+
+    res.status(204).end();
+  } catch {
+    res.status(500).json({ ok: false });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
