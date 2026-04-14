acd0eee9a9f641a06eaaf1171b57d944b31884b9
Akira Takagi <tkgakrwork@gmail.com>
2026-04-14 21:29:58 +0900

    19-8-5-3 DeleteReviewService をドメインイベントを発行するように変更

--- CatalogService/src/Application/Review/DeleteReviewService/DeleteReviewService.ts
+++ CatalogService/src/Application/Review/DeleteReviewService/DeleteReviewService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from "tsyringe";
 
+import { IDomainEventPublisher } from "Application/shared/DomainEvent/IDomainEventPublisher";
 import { ITransactionManager } from "Application/shared/ITransactionManager";
 import { IReviewRepository } from "Domain/models/Review/IReviewRepository";
 import { ReviewId } from "Domain/models/Review/ReviewId/ReviewId";
@@ -15,10 +16,12 @@ export class DeleteReviewService {
     private reviewRepository: IReviewRepository,
     @inject("ITransactionManager")
     private transactionManager: ITransactionManager,
+    @inject("IDomainEventPublisher")
+    private domainEventPublisher: IDomainEventPublisher,
   ) {}
 
   async execute(command: DeleteReviewCommand): Promise<void> {
-    await this.transactionManager.begin(async () => {
+    const review = await this.transactionManager.begin(async () => {
       // 対象のレビューを取得して存在確認
       const reviewId = new ReviewId(command.reviewId);
       const review = await this.reviewRepository.findById(reviewId);
@@ -27,7 +30,17 @@ export class DeleteReviewService {
         throw new Error("レビューが存在しません");
       }
 
+      review.delete();
+
       await this.reviewRepository.delete(reviewId);
+
+      return review;
     });
+
+    const events = review.getDomainEvents();
+    for (const event of events) {
+      this.domainEventPublisher.publish(event);
+    }
+    review.clearDomainEvents();
   }
 }
