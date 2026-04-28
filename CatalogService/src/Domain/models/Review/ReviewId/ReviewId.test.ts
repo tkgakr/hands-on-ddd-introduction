71745a8847973c6029529c4db6c4314fdb0d0fa4
Akira Takagi <tkgakrwork@gmail.com>
2026-04-28 21:46:59 +0900

    test: ReviewIdテストでモック適用のため動的importに変更

--- CatalogService/src/Domain/models/Review/ReviewId/ReviewId.test.ts
+++ CatalogService/src/Domain/models/Review/ReviewId/ReviewId.test.ts
@@ -1,23 +1,29 @@
-import { ReviewId } from "./ReviewId";
-
-// nanoid() をモックする
 jest.mock("nanoid", () => ({
   nanoid: () => "testIdWithExactLength",
 }));
 
 describe("ReviewId", () => {
-  test("デフォルトの値でReviewIdを生成する", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test("デフォルトの値でReviewIdを生成する", async () => {
+    // setupJest.ts 経由で ReviewId が先に評価されるため、モック適用後の状態で
+    // 再読込できるように各テスト内で動的 import している。
+    const { ReviewId } = await import("./ReviewId");
     const reviewId = new ReviewId();
     expect(reviewId.value).toBe("testIdWithExactLength");
   });
 
-  test("指定された値でReviewIdを生成する", () => {
+  test("指定された値でReviewIdを生成する", async () => {
+    const { ReviewId } = await import("./ReviewId");
     const value = "customId";
     const reviewId = new ReviewId(value);
     expect(reviewId.value).toBe(value);
   });
 
-  test("最小長以下の値でReviewIdを生成するとエラーを投げる", () => {
+  test("最小長以下の値でReviewIdを生成するとエラーを投げる", async () => {
+    const { ReviewId } = await import("./ReviewId");
     const shortValue = "";
     expect(() => new ReviewId(shortValue)).toThrowError(
       new Error(
@@ -26,7 +32,8 @@ describe("ReviewId", () => {
     );
   });
 
-  test("最大長以上の値でReviewIdを生成するとエラーを投げる", () => {
+  test("最大長以上の値でReviewIdを生成するとエラーを投げる", async () => {
+    const { ReviewId } = await import("./ReviewId");
     const longValue = "a".repeat(ReviewId.MAX_LENGTH + 1);
     expect(() => new ReviewId(longValue)).toThrowError(
       new Error(
